0c1c6936fb2f958969e1c35eb35d09826ada7f84
Sukurain <naibai321@gmail.com>
2025-01-05 15:08:18 +0800

    Update quickmiao-ad-blocker.js

--- quickmiao-ad-blocker.js
+++ quickmiao-ad-blocker.js
@@ -1,22 +1,18 @@
-const adDomains = [
-    'https://ad.xmmnsd.com/uploads/images/',
-    'https://69vvnstttaaa888.dzlndygh.com/i/',
-    'https://hongniu.getehu.com/i/',
-    'https://ad.xmmnsl.com/uploads/images/'
-];
+// remove-ad.js
 
-const removeElements = () => {
-    const imgs = $response.body.match(/<img[^>]*src=["']([^"']+)["']/g);
-    if (imgs) {
-        imgs.forEach(img => {
-            adDomains.forEach(domain => {
-                if (img.includes(domain)) {
-                    $response.body = $response.body.replace(img, '');
-                }
-            });
-        });
-    }
-    return $response.body;
-};
+var body = $response.body;
+var url = $request.url;
 
-removeElements();
+// 检查广告域名并进行处理
+if (url.includes("ad.xmmnsd.com")) {
+  body = body.replace(/<img[^>]*src="https:\/\/ad\.xmmnsd\.com\/uploads\/images\/[^"]*"[^>]*>/g, '');
+  body = body.replace(/<a[^>]*href="https:\/\/ad\.xmmnsd\.com[^"]*"[^>]*>/g, '');
+} else if (url.includes("dzlndygh.com")) {
+  body = body.replace(/<img[^>]*src="https:\/\/69vvnstttaaa888\.dzlndygh\.com\/i\/[^"]*"[^>]*>/g, '');
+} else if (url.includes("getehu.com")) {
+  body = body.replace(/<img[^>]*src="https:\/\/hongniu\.getehu\.com\/i\/[^"]*"[^>]*>/g, '');
+} else if (url.includes("xmmnsl.com")) {
+  body = body.replace(/<img[^>]*src="https:\/\/ad\.xmmnsl\.com\/uploads\/images\/[^"]*"[^>]*>/g, '');
+}
+
+$done({body: body});
